82d0eab641460e6507347067f1a6a43c68a7a60c
José Góngora <jdavidgongorao@gmail.com>
2023-08-17 14:45:30 -0600

    Se agrego el atributo nombre_senal

--- dato.py
+++ dato.py
@@ -1,6 +1,7 @@
 class dato:
-    def __init__(self, tiempo, amplitud, valor, valor_binario):
+    def __init__(self, tiempo, amplitud, valor, valor_binario, nombre_senal):
         self.tiempo = tiempo
         self.amplitud = amplitud
         self.valor = valor
         self.valor_binario = valor_binario
+        self.nombre_senal = nombre_senal
